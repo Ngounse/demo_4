c8f9eaa351d383550ba077833e94d1dea2bda03d
Ngounse <hengngoungse@gmail.com>
2022-06-13 15:18:21 +0700

    delete

--- djangonautic/ingredients/admin.py
+++
@@ -1,7 +0,0 @@
-
-# Register your models here.
-from django.contrib import admin
-from ingredients.models import Category, Ingredient
-
-admin.site.register(Category)
-admin.site.register(Ingredient)
